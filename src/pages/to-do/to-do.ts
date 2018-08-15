4426d195fc55de26829a17ecaae6338ac02a899a
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2018-08-15 17:19:37 +0530

    remove extra imports

--- src/pages/to-do/to-do.ts
+++ src/pages/to-do/to-do.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the ToDoPage page.
