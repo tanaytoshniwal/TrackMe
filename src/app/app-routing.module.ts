3812a5a033b20b50676e6e988a03f51af938f03c
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2019-06-17 02:51:36 +0530

    removing home and list entry

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
-  },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  },
+  { path: '', redirectTo: 'todo', pathMatch: 'full' },
   { path: 'todo', loadChildren: './todo/todo.module#TodoPageModule' },
   { path: 'transactions', loadChildren: './transactions/transactions.module#TransactionsPageModule' },
   { path: 'notes', loadChildren: './notes/notes.module#NotesPageModule' },
