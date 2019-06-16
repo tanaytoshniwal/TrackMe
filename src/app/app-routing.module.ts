e43940b2c683b6d7574924a29a76d89171de8002
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2019-06-17 01:39:48 +0530

    starter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'todo', loadChildren: './todo/todo.module#TodoPageModule' }
 ];
 
 @NgModule({
